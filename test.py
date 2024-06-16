0313c5a9f3495a8925ed41630d6fdc5882382e03
fastmath <andyrights37@gmail.com>
2024-06-16 14:30:09 +0530

    output matching + good enough sampling

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ model_hf = AutoModelForCausalLM.from_pretrained(repo).to(device)
 model_custom = GPT.from_pretrained('gpt2').to(device)
 
 tokens_hf = model_hf.generate(encoded, max_length=128, do_sample=False)
-tokens_custom = model_custom.generate(encoded, 128, temperature=1.0)
+tokens_custom = model_custom.generate(encoded, 128, temperature=1.0, check=True)
 print("---------")
 print(tokens_hf.shape, tokens_hf)
 print(tokens_custom.shape, tokens_custom)
